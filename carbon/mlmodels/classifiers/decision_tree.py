1e8f4e3c61317dc89aca75c91c8780c045d11448
Rajeev Ranjan <ranjanrajee@gmail.com>
2021-01-16 06:39:27 +0530

    working decision tree classifier backend

--- carbon/mlmodels/classifiers/decision_tree.py
+++ carbon/mlmodels/classifiers/decision_tree.py
@@ -18,7 +18,7 @@ from carbon.mlmodels.utils import (
 
 # from pprint import pprint
 # from datetime import datetime
-from carbon.mlmodels._test_config import finalConfig1
+from confgin import finalconfig1
 
 
 def build(confign, model_config=None):
@@ -36,10 +36,9 @@ def build(confign, model_config=None):
     X_train, X_test, Y_train, Y_test = splitTrainTestdataset(X, Y, config)
 
     # print("start", datetime.now())
-    if model_config:
-        clf_gini, clf_gini_fit = gridSearchDecisionTreeClf(X_train, Y_train, config)
-    else:
-        clf_gini, clf_gini_fit = gridSearchDecisionTreeClf(X_train, Y_train, config, model_config)
+    # pprint(model_config)
+
+    clf_gini, clf_gini_fit = gridSearchDecisionTreeClf(X_train, Y_train, config, model_config)
     # print("end", datetime.now())
 
     # Model Evaluation
@@ -97,7 +96,7 @@ def rocCurveforMultiClassDecisionTree(X, Y, catClasses, clfObject):
 
 
 def gridSearchDecisionTreeClf(X, Y, config, model_config=None):
-    if model_config:
+    if not model_config:
         gsClf = GridSearchCV(
             DecisionTreeClassifier(criterion="gini", random_state=100),
             param_grid={
@@ -106,25 +105,27 @@ def gridSearchDecisionTreeClf(X, Y, config, model_config=None):
                 # "max_depth": range(2, len(finalFeatureListGenerator(config)), 2),
             },
             cv=config["data"]["cv"]["folds"],
-            return_train_score=True
-        )
+            return_train_score=True)
         gsClf_fit = gsClf.fit(X, Y)
-        gsclf_results = [gsClf_fit.cv_results_, gsClf_fit.best_estimator_,
-                         gsClf_fit.best_score_, gsClf_fit.best_params_, gsClf_fit.best_index_]
+        gsclf_results = [
+            gsClf_fit.cv_results_, gsClf_fit.best_estimator_, gsClf_fit.best_score_, gsClf_fit.best_params_,
+            gsClf_fit.best_index_
+        ]
         pprint(gsclf_results)
         gsClf_fit_estimator = gsClf_fit.best_estimator_
         # print(gsClf_fit.best_params_, gsClf_fit.best_score_)
         return gsClf, gsClf_fit_estimator
     else:
-        gsClf = GridSearchCV(
-            DecisionTreeClassifier(random_state=100),
-            param_grid=model_config,
-            cv=config["data"]["cv"]["folds"],
-            return_train_score=True
-        )
+        # print(model_config)
+        gsClf = GridSearchCV(DecisionTreeClassifier(random_state=100),
+                             param_grid=model_config,
+                             cv=config["data"]["cv"]["folds"],
+                             return_train_score=True)
         gsClf_fit = gsClf.fit(X, Y)
-        gsclf_results = [gsClf_fit.cv_results_, gsClf_fit.best_estimator_,
-                         gsClf_fit.best_score_, gsClf_fit.best_params_, gsClf_fit.best_index_]
+        gsclf_results = [
+            gsClf_fit.cv_results_, gsClf_fit.best_estimator_, gsClf_fit.best_score_, gsClf_fit.best_params_,
+            gsClf_fit.best_index_
+        ]
         pprint(gsclf_results)
         gsClf_fit_estimator = gsClf_fit.best_estimator_
         # print(gsClf_fit.best_params_, gsClf_fit.best_score_)
@@ -132,24 +133,27 @@ def gridSearchDecisionTreeClf(X, Y, config, model_config=None):
 
 
 default_hyperparameters = {
-    "criterion": "gini",
+    "criterion": ["gini"],
     # criterion:{“gini”, “entropy”}, default=”gini”
     # The function to measure the quality of a split. Supported criteria are “gini” for the Gini impurity and “entropy” for the information gain.
-    "splitter": "best",  # splitter:{“best”, “random”}, default=”best”
+    "splitter": ["best"],  # splitter:{“best”, “random”}, default=”best”
     # The strategy used to choose the split at each node. Supported strategies are “best” to choose the best split and “random” to choose the best random split.
-    "max_depth": None,  # max_depth: int, default=None
+    "max_depth": [None],  # max_depth: int, default=None
     # The maximum depth of the tree. If None, then nodes are expanded until all leaves are pure or until all leaves contain less than min_samples_split samples.
-    "min_samples_split": 2,  # "min_samples_split":int or float, default=2
+    "min_samples_split": [2],  # "min_samples_split":int or float, default=2
     # The minimum number of samples required to split an internal node:
-    "min_samples_leaf": 1,  # min_samples_leaf: int or float, default=1
+    "min_samples_leaf": [1],  # min_samples_leaf: int or float, default=1
     # The minimum number of samples required to be at a leaf node. A split point at any depth will only be considered if it leaves at least min_samples_leaf training samples in each of the left and right branches.
-    "min_weight_fraction_leaf": 0.0,  # min_weight_fraction_leaf: float, default=0.0
+    "min_weight_fraction_leaf": [0.0],  # min_weight_fraction_leaf: float, default=0.0
     # The minimum weighted fraction of the sum total of weights (of all the input samples) required to be at a leaf node. Samples have equal weight when sample_weight is not provided.
-    "max_features": None,  # max_features: int, float or {“auto”, “sqrt”, “log2”}, default=None
+    "max_features": [None],  # max_features: int, float or {“auto”, “sqrt”, “log2”}, default=None
     # The number of features to consider when looking for the best split:
-    "max_leaf_nodes": None,  # max_leaf_nodes: int, default=None
+    "max_leaf_nodes": [None],  # max_leaf_nodes: int, default=None
     # Grow a tree with max_leaf_nodes in best-first fashion. Best nodes are defined as relative reduction in impurity. If None then unlimited number of leaf nodes.
 }
+model_config1 = default_hyperparameters
+model_config1["criterion"] = ["gini", "entropy"]
+model_config1["max_features"] = range(10, finalconfig1["data"]["data"]["includedFeatures"], 10)
+model_config1["min_samples_split"]: range(2, 32, 10)
 
-
-pprint(build(finalConfig1))
+build(finalconfig1, model_config=model_config1)
