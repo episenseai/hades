74cd8a2bbe71cf5d422af4cdba47a08bb24eb87e
Sushant Kumar <sushant.mithila@gmail.com>
2021-06-11 23:17:52 +0530

    minor mod

--- scripts/format.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -e
-set -x
-
-# autoflake --remove-all-unused-imports --recursive --remove-unused-variables --in-place carbon tests scripts --exclude=__init__.py
-black carbon tests -l 120
-isort carbon tests scripts
