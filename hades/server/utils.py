a31433a98950e723f034668484ed84d4a570e608
Sushant Kumar <sushant.mithila@gmail.com>
2021-07-27 06:31:53 +0530

    cleanup

--- hades/server/utils.py
+++ hades/server/utils.py
@@ -21,20 +21,3 @@ class StrictBaseModel(BaseModel):
         validate_all = True
         # whether to ignore, allow, or forbid extra attributes during model initialization.
         extra = "forbid"
-
-
-def printBox(text):
-    """Print CLI Box around given text string"""
-    times = 4
-    boxVertical = "│"
-    boxHorizontal = "─"
-    boxTopLeft = "┌%s" % (boxHorizontal * times)
-    boxTopRight = "%s┐" % (boxHorizontal * times)
-    boxBottomLeft = "└%s" % (boxHorizontal * times)
-    boxBottomRight = "%s┘" % (boxHorizontal * times)
-    boxMiddleLeft = "%s%s" % (boxVertical, " " * times)
-    boxMiddleRight = "%s%s" % (" " * times, boxVertical)
-    lentext = len(text)
-    print("%s%s%s" % (boxTopLeft, boxHorizontal * lentext, boxTopRight))
-    print("%s%s%s" % (boxMiddleLeft, text, boxMiddleRight))
-    print("%s%s%s" % (boxBottomLeft, boxHorizontal * lentext, boxBottomRight))
