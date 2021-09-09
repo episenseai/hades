ddd78eea100e7a74aadc6ca919a2c9bce44c5740
Rajeev Ranjan <ranjanrajee@gmail.com>
2021-09-09 17:41:24 +0530

    print env variables

--- hades/mlmodels/main.py
+++ hades/mlmodels/main.py
@@ -1,9 +1,11 @@
 from .app import join_model_workers, kill_model_workers, spawn_model_workers
+from .env import env
 
 if __name__ == "__main__":
     try:
         spawn_model_workers()
         print("============ STARTING (MLMODELS) ============")
+        print(f"{env()}")
         join_model_workers()
     except KeyboardInterrupt:
         pass
